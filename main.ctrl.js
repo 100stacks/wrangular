bec583146295b79e672ee5d41e310beba5862d21
site-demo <site9demo@monumentmail.com>
2015-07-08 23:53:47 -0500

    fix comments and formatting

--- main.ctrl.js
+++ main.ctrl.js
@@ -2,7 +2,7 @@
  * Angular controller - the business logic of an Angular application
  *
  * @desc Before we can use the contoller we need to instruct Angular where to implement it in our HTML document(s)
- * 		 We can have multiple Controllers per template and we can be nested controllers
+ * 		 We can have multiple Controllers per template and we can have nested controllers
  */
 
  // The controller is were your business logic should be handled, essentially the controller CONTROLS
@@ -12,6 +12,7 @@
 //	a new controller.
 //		- 1st parameter:  The name of our controller
 //		- 2nd parameter:  The controller logic
+
  angular.module('app').controller("MainController", function() {
  	// To avoid $scope issues, we capture the controller instance with 'vm' and manipulate it instead of global $scope
  	var vm = this;
